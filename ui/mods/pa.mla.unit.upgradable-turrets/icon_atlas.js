81101708d43d451704d37c07ffa480161951b4e6
BotWhan <68204459+BotWhan@users.noreply.github.com>
2022-08-09 12:42:10 -0400

    Add files via upload

---
+++ ui/mods/pa.mla.unit.upgradable-turrets/icon_atlas.js
@@ -0,0 +1,35 @@
+var paeiouIcons = [
+    "tree_hardpoint_base",
+
+    "build",
+
+    "tier_1_machine_gun",
+    "tier_1_machine_gun_build",
+
+    "tier_2_machine_gun",
+    "tier_2_machine_gun_build",
+
+    "tier_3_machine_gun",
+    "tier_3_machine_gun_build",
+
+    "tier_2_gatling",
+    "tier_2_gatling_build",
+
+    "tier_1_cannon",
+
+    "tier_2_auto_cannon",
+
+    "tier_3_artillery",
+
+    "tier_3_rail_cannon",
+
+    "edison",
+    "edison_build",
+
+    "tier_1_radar",
+    "tier_1_radar_build",
+
+    "tier_2_radar",
+    "tier_2_radar_build"
+];
+model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
---
+++ ui/mods/pa.mla.unit.upgradable-turrets/shared_build.js
@@ -0,0 +1,36 @@
+var newBuild = {
+    
+    "/pa/units/addon/tree_hardpoint_base/tree_hardpoint_base.json": ["utility", 0, { row: 1, column: 0 }],
+    
+    
+    "/pa/units/addon/tier_1_machine_gun_build/tier_1_machine_gun_build.json": ["tier 1", 0, { row: 2, column: 0 }],
+
+    "/pa/units/addon/tier_1_cannon_build/tier_1_cannon_build.json": ["tier 1", 0, { row: 2, column: 1 }],
+    "/pa/units/addon/tier_2_auto_cannon_build/tier_2_auto_cannon_build.json": ["cannon", 0, { row: 2, column: 0 }],
+    "/pa/units/addon/tier_3_artillery_build/tier_3_artillery_build.json": ["tier 3 arty", 0, { row: 2, column: 0 }],
+    "/pa/units/addon/tier_3_rail_cannon_build/tier_3_rail_cannon_build.json": ["cannon", 0, { row: 2, column: 1 }],
+    
+    // "/pa/units/addon/tier_1_rocket_build/tier_1_rocket_build.json": ["tier 1", 0, { row: 2, column: 2 }],
+
+    "/pa/units/addon/tier_1_radar_build/tier_1_radar_build.json": ["tier 1", 0, { row: 2, column: 2 }],
+    "/pa/units/addon/tier_2_radar_build/tier_2_radar_build.json": ["tier 2 radar", 0, { row: 2, column: 0 }],
+
+    "/pa/units/addon/tier_2_machine_gun_build/tier_2_machine_gun_build.json": ["mg2", 0, { row: 2, column: 0 }],
+
+    "/pa/units/addon/tier_2_gatling_build/tier_2_gatling_build.json": ["mg4", 0, { row: 2, column: 1 }],
+    
+
+
+
+    "/pa/units/addon/tree_rocket_base_build/tree_rocket_base_build.json": ["basic", 0, { row: 2, column: 3 }],
+
+    "/pa/units/addon/edison_build/edison_build.json": ["edison", 0, { row: 2, column: 0 }],
+    
+    "/pa/units/addon/tier_3_machine_gun_build/tier_3_machine_gun_build.json": ["mg4", 0, { row: 2, column: 2 }],
+
+    // "/pa/units/addon/tier_3_beamer_build/tier_3_beamer_build.json": ["cannon", 0, { row: 2, column: 1 }],
+
+}
+if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
+    _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
+}
