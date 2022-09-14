99f99e136af58c8a9a5ef341615b6285231d3ff9
BotWhan <68204459+BotWhan@users.noreply.github.com>
2022-09-14 12:46:09 -0400

    Update shared_build.js

--- ui/mods/pa.mla.unit.upgradable-turrets/shared_build.js
+++ ui/mods/pa.mla.unit.upgradable-turrets/shared_build.js
@@ -1,6 +1,7 @@
 var newBuild = {
     
     "/pa/units/addon/tree_hardpoint_base/tree_hardpoint_base.json": ["hardpoint", 0, { row: 2, column: 0 }],
+    "/pa/units/addon/tree_hardpoint_base_legion/tree_hardpoint_base_legion.json": ["L_hardpoint", 0, { row: 2, column: 0 }],
     
     
     "/pa/units/addon/tier_1_machine_gun_build/tier_1_machine_gun_build.json": ["tier 1", 0, { row: 2, column: 0 }],
